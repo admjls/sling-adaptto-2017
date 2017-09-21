3d5d1a07335f12dbb2e7f757b1b84aad4f1ea70e
Bertrand Delacretaz <bdelacretaz@apache.org>
2017-09-21 16:00:37 +0200

    Aggregation using SlingQuery

--- bundles/aggregator/src/main/java/ch/x42/at17/aggregator/ContentBVP.java
+++ bundles/aggregator/src/main/java/ch/x42/at17/aggregator/ContentBVP.java
@@ -6,13 +6,19 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptException;
 import javax.servlet.Servlet;
 import org.apache.commons.io.IOUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.scripting.SlingScript;
 import org.apache.sling.api.servlets.ServletResolver;
 import org.apache.sling.api.wrappers.SlingHttpServletRequestWrapper;
+import org.apache.sling.query.SlingQuery;
+import org.apache.sling.query.api.SearchStrategy;
 import org.apache.sling.scripting.api.BindingsValuesProvider;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -28,23 +34,41 @@ public class ContentBVP implements BindingsValuesProvider {
     @Reference
     private ServletResolver servletResolver;
     
+    @Reference(target="(names=javascript)")
+    private ScriptEngineFactory javascriptEngineFactory;
+    
+    public class DollarAdapter {
+        public SlingQuery $(Resource r) {
+            return SlingQuery.$(r);
+        }
+        public SlingQuery $(ResourceResolver resolver) {
+            return SlingQuery.$(resolver);
+        }
+    }
+    
     @Override
     public void addBindings(Bindings b) {
         final Resource resource = (Resource)b.get("resource");
         final SlingHttpServletRequest request = (SlingHttpServletRequest)b.get("request");
         if(resource != null && request != null) {
-            b.put("content", getContent(resource, request, b));
+            try {
+                b.put("content", aggregateContent(resource, getAggregatorScript(request)));
+            } catch(Exception e) {
+                throw new RuntimeException("Content aggregation failed", e);
+            }
         }
     }
     
-    /** Get aggregated content for Resource r */
-    private Object getContent(Resource r, SlingHttpServletRequest request, Bindings b) {
-        final Map<String, Object> content = new HashMap<>();
-        content.put(RESOURCE_KEY, r.getPath());
-        content.put("now", new Date());
-        content.put("path", request.getRequestPathInfo().getResourcePath());
-        content.put("script", getText(getAggregatorScript(request)));
-        return content;
+    private Object aggregateContent(Resource r, SlingScript script) throws ScriptException {
+        final String text = getText(script);
+        final ScriptEngine e = javascriptEngineFactory.getScriptEngine();
+        final Bindings b = e.createBindings();
+        b.put("query", new DollarAdapter());
+        b.put("SearchStrategyQUERY", SearchStrategy.QUERY);
+        b.put("resource", r);
+        b.put("resourceResolver", r.getResourceResolver());
+        final Object result = e.eval(text, b);
+        return result;
     }
     
     private SlingScript getAggregatorScript(SlingHttpServletRequest request) {
--- bundles/aggregator/test-scripts/create-content.sh
+++ bundles/aggregator/test-scripts/create-content.sh
@@ -4,4 +4,6 @@ export U=admin:admin
 curl -u $U -Fsling:resourceType=agg $BASE/apps/agg ; echo
 curl -u $U -T agg.esp $BASE/apps/agg/agg.esp ; echo
 curl -u $U -T SLING-CONTENT.esp $BASE/apps/agg/SLING-CONTENT.esp ; echo
+
+echo Script output:
 curl -u $U $BASE/apps/agg.html ; echo
